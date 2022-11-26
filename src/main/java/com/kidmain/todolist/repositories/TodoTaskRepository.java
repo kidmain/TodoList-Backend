a686e1f2e00f1da70381b31ae89b9ec756d95add
Kidmain <thekidmain@gmail.com>
2022-11-26 19:59:25 +0300

    Add log to UserController

---
+++ src/main/java/com/kidmain/todolist/repositories/TodoTaskRepository.java
@@ -0,0 +1,13 @@
+package com.kidmain.todolist.repositories;
+
+import com.kidmain.todolist.entities.TodoTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoTaskRepository extends JpaRepository<TodoTask, Long> {
+    List<TodoTask> findAllByUserId(Long id);
+    List<TodoTask> findAllByOrderById();
+}
