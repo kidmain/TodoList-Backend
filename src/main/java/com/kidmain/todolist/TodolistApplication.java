ee51083bf31b69ff0621d9a48f3bddb925b4f4d3
Kidmain <thekidmain@gmail.com>
2022-12-06 17:09:05 +0300

    CORS fix

--- src/main/java/com/kidmain/todolist/TodolistApplication.java
+++ src/main/java/com/kidmain/todolist/TodolistApplication.java
@@ -2,9 +2,8 @@ package com.kidmain.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class TodolistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
